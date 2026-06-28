27c472109d0073382cec6910993d4696d56f9b07
QuisTech <michquis@gmail.com>
2026-06-28 19:02:18 +0100

    fix: resolve editor scroll and height constraints

--- src/components/CodeViewer.tsx
+++ src/components/CodeViewer.tsx
@@ -53,19 +53,20 @@ export default function CodeViewer({ files }: { files: Record<string, string> })
           classes: {
             "sp-layout": "h-full w-full !border-0 !rounded-none",
             "sp-file-explorer": "bg-[#12141a]",
-            "sp-editor": "border-r border-white/5",
-            "sp-preview": "bg-white",
-          }
+            "sp-editor": "border-r border-white/5 h-full",
+            "sp-preview": "bg-white h-full",
+          },
+          editorHeight: "100%"
         }}
       >
         <SandpackLayout className="h-full w-full flex">
           <div className="w-48 shrink-0 overflow-y-auto border-r border-white/5">
              <SandpackFileExplorer autoHiddenFiles />
           </div>
-          <div className="flex-1 relative min-w-0">
-             <SandpackCodeEditor showTabs={true} showLineNumbers={true} style={{ height: "100%" }} />
+          <div className="flex-1 relative min-w-0 h-full overflow-hidden">
+             <SandpackCodeEditor showTabs={true} showLineNumbers={true} wrapContent={true} style={{ height: "100%" }} />
           </div>
-          <div className="flex-1 relative min-w-0 border-l border-white/5">
+          <div className="flex-1 relative min-w-0 border-l border-white/5 h-full overflow-hidden">
              <SandpackPreview showNavigator={true} showOpenInCodeSandbox={false} style={{ height: "100%" }} />
           </div>
         </SandpackLayout>
