e00e98286f9c0d5f10e27dff9951a2c7e9072178
QuisTech <michquis@gmail.com>
2026-06-29 11:31:19 +0100

    fix: revert absolute hack and apply min-h-0 flex constraint for Sandpack layout

--- src/components/CodeViewer.tsx
+++ src/components/CodeViewer.tsx
@@ -59,20 +59,14 @@ export default function CodeViewer({ files }: { files: Record<string, string> })
         }}
       >
         <SandpackLayout className="h-full w-full flex">
-          <div className="w-48 shrink-0 relative">
-            <div className="absolute inset-0 overflow-y-auto border-r border-white/5">
-              <SandpackFileExplorer autoHiddenFiles />
-            </div>
+          <div className="w-48 shrink-0 overflow-y-auto border-r border-white/5">
+            <SandpackFileExplorer autoHiddenFiles />
           </div>
-          <div className="flex-1 relative min-w-0 h-full">
-            <div className="absolute inset-0">
-              <SandpackCodeEditor showTabs={true} showLineNumbers={true} wrapContent={true} style={{ height: "100%" }} />
-            </div>
+          <div className="flex-1 relative min-w-0 min-h-0 h-full overflow-hidden flex flex-col">
+            <SandpackCodeEditor showTabs={true} showLineNumbers={true} wrapContent={true} style={{ height: "100%" }} />
           </div>
-          <div className="flex-1 relative min-w-0 h-full">
-            <div className="absolute inset-0 border-l border-white/5">
-              <SandpackPreview showNavigator={true} showOpenInCodeSandbox={false} style={{ height: "100%" }} />
-            </div>
+          <div className="flex-1 relative min-w-0 min-h-0 border-l border-white/5 h-full overflow-hidden flex flex-col">
+            <SandpackPreview showNavigator={true} showOpenInCodeSandbox={false} style={{ height: "100%" }} />
           </div>
         </SandpackLayout>
       </SandpackProvider>
